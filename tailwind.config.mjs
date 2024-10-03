8a08a8ea227f187a8b46f7194cd1a2c97802d538
Brian Nieto <brian.nieto1@utp.ac.pa>
2024-10-02 20:06:50 -0500

    Global styles config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				main: '#FCFCFC',
+				secondary: '#E4E4E4',
+				contrast: '#001234',
+				font: '#1C1C1C',
+				fontInvert: '#FCFCFC'
+			},
+			borderRadius: {
+				global: '15px'
+			}
+		},
 	},
 	plugins: [],
 }
