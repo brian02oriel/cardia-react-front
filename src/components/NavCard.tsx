38177782cbe0858753b0f835b0c2f07214d0087e
Brian Nieto <brian.nieto1@utp.ac.pa>
2024-10-07 23:00:10 -0500

    Adding responsive styles

--- src/components/NavCard.tsx
+++ src/components/NavCard.tsx
@@ -15,7 +15,6 @@ const NavCard: React.FC<NavCardProps> = ({ type, label, width, height, iconWidth
     return (
         <div className='flex flex-col items-center justify-center 
                         p-4  border-4 border-contrast rounded-custom 
-                        text-contrast
                         transition-transform duration-300 ease-in-out transform cursor-pointer
                         focus:animate-pulse
                          hover:text-main hover:scale-105 hover:bg-contrast hover:shadow-xl hover:shadow-contrast' style={{ width: width,  height: height}}>
@@ -26,7 +25,7 @@ const NavCard: React.FC<NavCardProps> = ({ type, label, width, height, iconWidth
                 type === 'chart' ? <Chart width={iconWidth} height={iconHeight}/> :
                 <Settings width={iconWidth} height={iconHeight}/>}
                 <div>
-                    <h2>{label}</h2>
+                    <p className='sm:text-2xl'>{label}</p>
                 </div>
         </div>
     );
